7956eda8a3064125912fcc8967d235ebac81908e
Analee Guerra <aguerra1508@gmail.com>
2020-09-17 22:16:22 -0500

    Moved questions to its own section

--- index.js
+++ index.js
@@ -1,48 +1,51 @@
+// array of questions for user
+const questions = [
+  {
+    type: "input",
+    message: "What is your project title?",
+    name: "title"
+  },
+  {
+    type: "input",
+    message: "Enter a description for your project.",
+    name: "description"
+  },
+  {
+    type: "input",
+    message: "Enter installation requirements for your project.",
+    name: "installation"
+  },
+  {
+    type: "input",
+    message: "Enter how your project should be used.",
+    name: "usage"
+  },
+  {
+    type: "input",
+    message: "Would you like to include the contributors covenant?",
+    name: "contributor"
+  },
+  {
+    type: "input",
+    message: "Enter any tests on how to use your project.",
+    name: "tests"
+  },
+  {
+    type: "input",
+    message: "What is your Git Hub user name?",
+    name: "username"
+  },
+  {
+    type: "email",
+    message: "What is your email",
+    name: "email"
+  },
+];
+
 var inquirer = require("inquirer");
 
     inquirer
-      .prompt([
-        {
-          type: "input",
-          message: "What is your project title?",
-          name: "title"
-        },
-        {
-          type: "input",
-          message: "Enter a description for your project.",
-          name: "description"
-        },
-        {
-          type: "input",
-          message: "Enter installation requirements for your project.",
-          name: "installation"
-        },
-        {
-          type: "input",
-          message: "Enter how your project should be used.",
-          name: "usage"
-        },
-        {
-          type: "input",
-          message: "Would you like to include the contributors covenant?",
-          name: "contributor"
-        },
-        {
-          type: "input",
-          message: "Enter any tests on how to use your project.",
-          name: "tests"
-        },
-        {
-          type: "input",
-          message: "What is your Git Hub user name?",
-          name: "username"
-        },
-        {
-          type: "email",
-          message: "What is your email",
-          name: "email"
-        },
-      ])
+      .prompt(questions)
   .then(answers => {
     // Use user feedback for... whatever!!
   })
@@ -53,12 +56,6 @@ var inquirer = require("inquirer");
       // Something else when wrong
     }
   });
-
-
-// array of questions for user
-const questions = [
-    
-];
 
 // function to write README file
 function writeToFile(fileName, data) {
